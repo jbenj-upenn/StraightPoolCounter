091947af5e02b5377a076e65907961f2def61a3e
Josh <joshuasevendavid@gmail.com>
2022-12-16 12:59:05 -0500

    remove commented out code:

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-// document.getElementById("count-el").innerText = 50;
 
-// let firstBatch = 5;
-// let secondBatch = 12;
-
-// let count = firstBatch + secondBatch;
-
-// console.log(count);
-
-// let count = 5;
-// count = count++;
-// console.log(count);
-
-// let bonusPoints = 50;
-// bonusPoints = bonusPoints + 50;
-// console.log(bonusPoints);
-// bonusPoints = bonusPoints - 75;
-// console.log(bonusPoints);
-// bonusPoints = bonusPoints + 45;
-
-// console.log(bonusPoints);
-
-// 
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 let runEl = document.getElementById("run-el")
